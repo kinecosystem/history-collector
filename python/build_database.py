2ac2f33922e1034adf108fa662e7d4775f10436a
ronserruya <ron.serruya@kik.com>
2018-10-31 11:29:08 +0200

    Update the create database script

--- python/build_database.py
+++ python/build_database.py
@@ -1,20 +1,21 @@
 """Script to build a database for main.py to write data to."""
 
 import os
+import sys
 import logging
 
 import psycopg2
 
 # Get constants from env variables
 PYTHON_PASSWORD = os.environ['PYTHON_PASSWORD']
-ASSET_CODE = os.environ['ASSET_CODE']
 FIRST_FILE = os.environ['FIRST_FILE']
 POSTGRES_PASSWORD = os.environ['POSTGRES_PASSWORD']
+POSTGRES_HOST = os.environ['POSTGRES_HOST']
 
 
 def setup_postgres(database=''):
     """Set up a connection to the postgres database."""
-    conn = psycopg2.connect("postgresql://postgres:{}@db:5432".format(POSTGRES_PASSWORD) + database)
+    conn = psycopg2.connect("postgresql://postgres:{}@{}:5432".format(POSTGRES_PASSWORD, POSTGRES_HOST) + database)
     conn.autocommit = True
     cur = conn.cursor()
     return cur
@@ -25,7 +26,7 @@ def main():
     logging.basicConfig(level='INFO', format='%(asctime)s | %(levelname)s | %(message)s')
     # Check if the database already exists
     try:
-        setup_postgres(database='/' + ASSET_CODE.lower())
+        setup_postgres(database='/kin')
     except psycopg2.OperationalError as e:
         if 'does not exist' in str(e):
             pass
@@ -33,32 +34,47 @@ def main():
             raise
     else:
         logging.info('Using existing database instead of creating a new one')
-        quit(0)
+        sys.exit(0)
 
     # Connect to the postgres database
     try:
         cur = setup_postgres()
 
         # Create the database
-        cur.execute('CREATE DATABASE {};'.format(ASSET_CODE.lower()))
+        cur.execute('CREATE DATABASE {};'.format('kin'))
 
         # Create the users
         cur.execute('CREATE USER python;')
         cur.execute("ALTER USER python WITH PASSWORD '{}'".format(PYTHON_PASSWORD))
 
         # Create the tables
-        cur = setup_postgres('/' + ASSET_CODE.lower())
-        cur.execute('CREATE TABLE payments(source varchar(255)'
-                    ',destination varchar(255),amount FLOAT,memo_text varchar(255),hash varchar(255),time TIMESTAMP);')
-        cur.execute('CREATE TABLE trustlines(source varchar(255),memo_text varchar(255), hash varchar(255),time TIMESTAMP);')
-        cur.execute('CREATE TABLE lastfile(name varchar(255));')
+        cur = setup_postgres('/kin')
+
+        cur.execute('CREATE TABLE payments('
+                    'source varchar(56) not NULL,'
+                    'destination varchar(56) not NULL,'
+                    'amount FLOAT not NULL,'
+                    'memo_text varchar(28),'
+                    'hash varchar(64) not NULL PRIMARY KEY,'
+                    'time TIMESTAMP not NULL);')
+
+        cur.execute('CREATE TABLE creations('
+                    'source varchar(56) not NULL,'
+                    'destination varchar(56) not NULL,'
+                    'starting_balance FLOAT not NULL,'
+                    'memo_text varchar(28),'
+                    'hash varchar(64) not NULL PRIMARY KEY,'
+                    'time TIMESTAMP not NULL);')
+
+        cur.execute('CREATE TABLE lastfile('
+                    'name varchar(8) not NULL);')
 
         # This is the name of the file that contains the first ledger to scan
-        cur.execute("INSERT INTO lastfile VALUES('{}');".format(FIRST_FILE))
+        cur.execute("INSERT INTO lastfile VALUES(%s);", (FIRST_FILE,))
 
         # Grant the users access to the database
         cur.execute('GRANT INSERT on payments TO python')
-        cur.execute('GRANT INSERT on trustlines TO python')
+        cur.execute('GRANT INSERT on creations TO python')
         cur.execute('GRANT INSERT on lastfile TO python')
         cur.execute('GRANT SELECT on lastfile TO python')
         cur.execute('GRANT UPDATE on lastfile to python')
