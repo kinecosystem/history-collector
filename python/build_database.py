bf8b8bfc7097eba7ea730dd55018e6d849a62732
David Bolshoy <bolshoy@gmail.com>
2018-12-09 19:14:59 +0200

    final fixes and improvements

--- python/build_database.py
+++ python/build_database.py
@@ -1,6 +1,9 @@
 """Script to build a database for main.py to write data to."""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import argparse
+import csv
 import os
 import sys
 import logging
@@ -8,15 +11,16 @@ import psycopg2
 
 logging.basicConfig(level='INFO', format='%(asctime)s | %(levelname)s | %(message)s')
 
-parser = argparse.ArgumentParser(description='Create database structure for history-collector')
-parser.add_argument('--host', default=os.environ.get('POSTGRES_HOST'), help='host')
-parser.add_argument('--password', default=os.environ.get('POSTGRES_PASSWORD'), help='password')
-parser.add_argument('--db', default='kin', help='database')
-parser.add_argument('--user', default='python', help='database user')
-parser.add_argument('--userpass', default=os.environ.get('PYTHON_PASSWORD'), help='user password')
-parser.add_argument('--first', default=os.environ.get('FIRST_FILE'), help='first file')
-parser.add_argument('--force', action='store_true', default=False, help='drop old db if exists!')
-args = parser.parse_args()
+argparser = argparse.ArgumentParser(description='Create database structure for history-collector')
+argparser.add_argument('--host', default=os.getenv('POSTGRES_HOST', 'localhost'), help='host')
+argparser.add_argument('--password', default=os.getenv('POSTGRES_PASSWORD', 'postgres'), help='password')
+argparser.add_argument('--db', default=os.getenv('DB_NAME', 'kin'), help='database')
+argparser.add_argument('--user', default=os.getenv('DB_USER', 'python'), help='database user')
+argparser.add_argument('--userpass', default=os.getenv('DB_USER_PASSWORD', '1234'), help='user password')
+argparser.add_argument('--first', default=os.getenv('FIRST_FILE', '0000003f'), help='first file')
+argparser.add_argument('--accounts', default='known_accounts.csv', help='known accounts csv filename')
+argparser.add_argument('--force', action='store_true', default=False, help='drop old db if exists!')
+args = argparser.parse_args()
 
 POSTGRES_HOST = args.host
 POSTGRES_PASSWORD = args.password
@@ -25,29 +29,34 @@ DB_USER = args.user
 DB_USER_PASSWORD = args.userpass
 FIRST_FILE = args.first
 FORCE_NEW_DB = args.force
+KNOWN_ACCOUNTS_FILE = args.accounts
 
-# Validate some arguments
 if (int(FIRST_FILE,16) + 1) % 64 != 0:
     logging.error('First file selected is invalid')
     sys.exit(1)
 
-def setup_postgres(database):
+
+def setup_postgres():
     """Set up a connection to the postgres database."""
     conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(POSTGRES_HOST, 'postgres', 'postgres', POSTGRES_PASSWORD)
     conn = psycopg2.connect(conn_string)
     conn.autocommit = True
     cur = conn.cursor()
 
-    # drop if needed
+    # drop older database if needed
     cur.execute("select exists(SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('{}'));".format(DB_NAME))
     db_exists = bool(cur.fetchone()[0])
     if db_exists:
         if FORCE_NEW_DB:
             logging.warning('Dropping existing database!')
+            cur.execute('SELECT pg_terminate_backend(pg_stat_activity.pid) '
+                        'FROM pg_stat_activity WHERE pg_stat_activity.datname = \'{}\' '
+                        'AND pid <> pg_backend_pid();'.format(DB_NAME))
             cur.execute('DROP DATABASE {};'.format(DB_NAME))
             cur.execute("DROP USER IF EXISTS {}".format(DB_USER))
         else:
-            raise Exception('database already exists, use --force flag')
+            logging.error('Database already exists, use --force flag to force dropping')
+            exit(1)
 
     # create our database
     cur.execute('CREATE DATABASE {};'.format(DB_NAME))
@@ -77,30 +86,27 @@ def main():
     """Main entry point."""
 
     try:
-        cur = setup_postgres(DB_NAME)
+        cur = setup_postgres()
 
-        # Create tables
+        # Create payments table
         cur.execute('CREATE TABLE payments('
                     'id SERIAL PRIMARY KEY,'
+                    'tx_hash varchar(64) not NULL,'
                     'account varchar(64) NOT NULL,'
                     'account_sequence bigint NOT NULL,'
                     'source varchar(64) not NULL,'
                     'destination varchar(64) not NULL,'
                     'amount FLOAT not NULL,'
                     'memo_text varchar(28),'
-                    'fee INT not NULL,'
-                    'fee_charged INT not NULL,'
-                    'op_status text,'
                     'op_index INT not NULL,'
-                    'tx_status text,'
-                    'tx_hash varchar(64) not NULL,'
                     'ledger_sequence INT NOT NULL,'
-                    'time TIMESTAMP without time zone not NULL);')
+                    'date TIMESTAMP without time zone not NULL);')
 
         cur.execute('CREATE INDEX by_account ON payments USING btree (account, account_sequence);')
         cur.execute('CREATE INDEX by_source ON payments USING btree (source);')
         cur.execute('CREATE INDEX by_destination ON payments USING btree (destination);')
         cur.execute('CREATE INDEX by_hash ON payments USING btree (tx_hash);')
+        cur.execute('CREATE UNIQUE INDEX by_op_in_tx ON payments USING btree (op_index, tx_hash);')
 
         cur.execute('GRANT ALL ON payments TO {}'.format(DB_USER))
 
@@ -117,39 +123,30 @@ def main():
                     'tx_status text,'
                     'op_status text,'
                     'hash varchar(64) not NULL,'
-                    'time TIMESTAMP not NULL);')
+                    'time TIMESTAMP WITHOUT TIME ZONE NOT NULL);')
                     
         cur.execute('GRANT ALL ON creations TO python')
         '''
 
-        '''https://github.com/stellar/horizon-importer/blob/master/db/structure.sql
-        CREATE TABLE history_transactions (
-            transaction_hash character varying(64) NOT NULL,
-            ledger_sequence integer NOT NULL,
-            application_order integer NOT NULL,
-            account character varying(64) NOT NULL,
-            account_sequence bigint NOT NULL,
-            fee_paid integer NOT NULL,
-            operation_count integer NOT NULL,
-            created_at timestamp without time zone,
-            updated_at timestamp without time zone,
-            id bigint,
-            tx_envelope text NOT NULL,
-            tx_result text NOT NULL,
-            tx_meta text NOT NULL,
-            tx_fee_meta text NOT NULL,
-            signatures character varying(96)[] DEFAULT '{}'::character varying[] NOT NULL,
-            memo_type character varying DEFAULT 'none'::character varying NOT NULL,
-            memo character varying,
-            time_bounds int8range
-        );
-        '''
+        # Create known_accounts table and fill it from csv
+        cur.execute('CREATE TABLE known_accounts ('
+                    'account varchar(64) PRIMARY KEY,'
+                    'description TEXT NOT NULL,'
+                    'network VARCHAR(64) NOT NULL);')
 
+        with open(KNOWN_ACCOUNTS_FILE, 'r') as f:
+            reader = csv.reader(f)
+            next(reader)  # Skip the header row.
+            for row in reader:
+                cur.execute("INSERT INTO known_accounts VALUES (%s, %s, %s)", row)
+
+        # Create and init lastfile table
         cur.execute('CREATE TABLE lastfile(name varchar(8) not NULL);')
 
         # This is the name of the file that contains the first ledger to scan
         cur.execute("INSERT INTO lastfile VALUES(%s);", (FIRST_FILE,))
 
+        # setup permissions
         cur.execute('GRANT ALL ON lastfile TO {}'.format(DB_USER))
 
         cur.execute('GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO {}'.format(DB_USER))
--- python/main.py
+++ python/main.py
@@ -5,7 +5,10 @@ Script to download xdr files from an s3 bucket,
 unpack them, filter the transactions in them,
 and write the relevant transactions to a database.
 """
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
+import argparse
 import os
 import time
 import logging
@@ -19,21 +22,40 @@ from botocore import UNSIGNED
 from botocore.client import Config
 from botocore.exceptions import ClientError
 import psycopg2
-from xdrparser import parser
+from psycopg2.extras import execute_values
+from xdrparser import parser as xdrparser
 
-# Get constants from env variables
-PYTHON_PASSWORD = os.getenv('PYTHON_PASSWORD', '1234')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
-KIN_ISSUER = os.getenv('KIN_ISSUER', 'GDF42M3IPERQCBLWFEZKQRK77JQ65SCKTU3CW36HZVCX7XX5A5QXZIVK')
-NETWORK_PASSPHARSE = os.getenv('NETWORK_PASSPHRASE', 'Public Global Kin Ecosystem Network ; June 2018')
-MAX_RETRIES = int(os.getenv('MAX_RETRIES', 5))
-BUCKET_NAME = os.getenv('BUCKET_NAME', 'stellar-core-ecosystem-6145')
-LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+logging.basicConfig(level='INFO', format='%(asctime)s | %(levelname)s | %(message)s')
 
-logging.basicConfig(level=LOG_LEVEL, format='%(asctime)s | %(levelname)s | %(message)s')
+argparser = argparse.ArgumentParser(description='history-collector - Stellar history importer')
+argparser.add_argument('--host', default=os.getenv('POSTGRES_HOST', 'localhost'), help='host')
+argparser.add_argument('--password', default=os.getenv('POSTGRES_PASSWORD', 'postgres'), help='password')
+argparser.add_argument('--db', default=os.getenv('DB_NAME', 'kin'), help='database')
+argparser.add_argument('--user', default=os.getenv('DB_USER', 'python'), help='database user')
+argparser.add_argument('--userpass', default=os.getenv('DB_USER_PASSWORD', '1234'), help='user password')
+argparser.add_argument('--issuer', default=os.getenv('KIN_ISSUER',
+                                                     'GDF42M3IPERQCBLWFEZKQRK77JQ65SCKTU3CW36HZVCX7XX5A5QXZIVK'),
+                       help='KIN issuer address')
+argparser.add_argument('--passphrase', default=os.getenv('NETWORK_PASSPHRASE',
+                                                         'Public Global Kin Ecosystem Network ; June 2018'),
+                       help='Stellar network passphrase')
+argparser.add_argument('--bucket', default=os.getenv('BUCKET_NAME', 'stellar-core-ecosystem-6145'), help='s3 bucket name')
+argparser.add_argument('--app', default=os.getenv('APP_ID'), help='database')
+argparser.add_argument('--retries', default=os.getenv('MAX_RETRIES', 5), help='max retries')
+argparser.add_argument('--dir', default=os.getenv('CORE_DIRECTORY', ''), help='working directory')
+args = argparser.parse_args()
 
-APP_ID = os.getenv('APP_ID', None)
-CORE_DIRECTORY = os.getenv('CORE_DIRECTORY', '')
+POSTGRES_HOST = args.host
+POSTGRES_PASSWORD = args.password
+DB_NAME = args.db
+DB_USER = args.user
+DB_USER_PASSWORD = args.userpass
+KIN_ISSUER = args.issuer
+NETWORK_PASSPHRASE = args.passphrase
+BUCKET_NAME = args.bucket
+APP_ID = args.app
+MAX_RETRIES = int(args.retries)
+CORE_DIRECTORY = args.dir
 
 # Add trailing / to core directory
 CORE_DIRECTORY = os.path.join(CORE_DIRECTORY, '')
@@ -41,6 +63,9 @@ CORE_DIRECTORY = os.path.join(CORE_DIRECTORY, '')
 # 1-<uppercase|lowercase|digits>*4-anything
 APP_ID_REGEX = re.compile('^1-[A-z0-9]{4}-.*')
 
+if APP_ID is not None and re.match('^[A-z0-9]{4}$', APP_ID) is None:
+    logging.error('APP ID is invalid')
+    sys.exit(1)
 
 def setup_s3():
     """Set up the s3 client with anonymous connection."""
@@ -51,18 +76,16 @@ def setup_s3():
 
 def setup_postgres():
     """Set up a connection to the postgres database using the user 'python'."""
-    conn = psycopg2.connect("postgresql://python:{}@{}:5432/kin".format(PYTHON_PASSWORD, POSTGRES_HOST))
+    conn_string = "host='{}' dbname='{}' user='{}' password='{}'".format(POSTGRES_HOST, DB_NAME, DB_USER, DB_USER_PASSWORD)
+    conn = psycopg2.connect(conn_string)
     logging.info('Successfully connected to the database')
     return conn
 
 
 def get_last_file_sequence(conn, cur):
     """Get the sequence of the last file scanned."""
-    cur.execute('select * from lastfile;')
-    conn.commit()
-    last_file = cur.fetchone()[0]
-
-    return last_file
+    cur.execute('SELECT * FROM lastfile;')
+    return cur.fetchone()[0]
 
 
 def download_file(s3, file_name):
@@ -74,7 +97,6 @@ def download_file(s3, file_name):
     file_number = file_name.split('-')[-1]
     sub_directory = '/'.join(file_number[i:i + 2] for i in range(0, len(file_number), 2))
     sub_directory = sub_directory[:9]
-
     sub_directory = file_name.split('-')[0] + '/' + sub_directory
 
     for attempt in range(MAX_RETRIES + 1):
@@ -112,10 +134,28 @@ def get_result_dictionary(results):
     return results_dict
 
 
-def write_to_postgres(conn, cur, transactions, ledgers_dictionary, results_dictionary, file_name):
+def get_app_id(tx_memo):
+    """Get app_id from transaction memo"""
+    if APP_ID_REGEX.match(str(tx_memo)):
+        return tx_memo.split('-')[1]
+    return None
+
+
+def is_asset_kin(asset):
+    return asset is not None \
+           and asset['type'] == 1 \
+           and asset['alphaNum4']['assetCode'] == 'KIN' \
+           and asset['alphaNum4']['issuer']['ed25519'] == KIN_ISSUER
+
+
+def write_to_postgres(conn, cur, history_transactions, ledgers_dictionary, results_dictionary, file_name):
     """Filter payment/creation operations and write them to the database."""
     logging.info('Writing contents of file: {} to database'.format(file_name))
-    for transaction_history_entry in transactions:
+
+    # aggregator
+    payments = []
+
+    for transaction_history_entry in history_transactions:
         ledger = ledgers_dictionary.get(transaction_history_entry['ledgerSeq'])
         timestamp = ledger['scpValue']['closeTime']
 
@@ -125,7 +165,7 @@ def write_to_postgres(conn, cur, transactions, ledgers_dictionary, results_dicti
             # Find the results of this tx based on its hash
             results = results_dictionary.get(tx_hash)
             
-            # Handle only successful transactions
+            # Handle only successful history_transactions
             tx_status = results['result']['code']  # txSUCCESS/FAILED/BAD_AUTH etc
             if tx_status != 'txSUCCESS':
                 logging.warning('skipping failed transaction {}'.format(tx_hash))
@@ -134,87 +174,51 @@ def write_to_postgres(conn, cur, transactions, ledgers_dictionary, results_dicti
             tx_memo = transaction['tx']['memo']['text']
 
             # If the transaction is not from our app, skip it
-            if APP_ID is not None:
-                if APP_ID_REGEX.match(str(tx_memo)) is not None:
-                    app = tx_memo.split('-')[1]
-                    if app != APP_ID:
-                        continue
-                else:
-                    continue
+            if APP_ID and get_app_id(tx_memo) != APP_ID:
+                continue
 
             tx_account = transaction['tx']['sourceAccount']['ed25519']
             tx_account_sequence = transaction['tx']['seqNum']
-            tx_fee = transaction['tx']['fee']
-            tx_charged_fee = results['feeCharged']
             tx_ledger_sequence = transaction_history_entry['ledgerSeq']
 
             for op_index, (tx_operation, result_operation) in enumerate(zip(transaction['tx']['operations'], results['result']['results'])):
-                # Operation type 1 = Payment
-                if tx_operation['body']['type'] == 1:
-                    # Check if this is a payment for our asset
-                    if tx_operation['body']['paymentOp']['asset']['alphaNum4'] is not None and \
-                                    tx_operation['body']['paymentOp']['asset']['alphaNum4']['assetCode'] == 'KIN' and \
-                                    tx_operation['body']['paymentOp']['asset']['alphaNum4']['issuer']['ed25519'] == KIN_ISSUER:
+                # Handle only payments
+                if tx_operation['body']['type'] != 1:
+                    continue
 
-                        source = transaction['tx']['sourceAccount']['ed25519']
-                        destination = tx_operation['body']['paymentOp']['destination']['ed25519']
-                        amount = tx_operation['body']['paymentOp']['amount']
-                        op_status = result_operation['tr']['paymentResult']['code']
+                # Handle only KIN payments
+                if not is_asset_kin(tx_operation['body']['paymentOp']['asset']):
+                    continue
 
-                        # Override the tx source with the operation source if it exists
-                        try:
-                            source = tx_operation['sourceAccount'][0]['ed25519']
-                        except (KeyError, IndexError):
-                            pass
+                source = transaction['tx']['sourceAccount']['ed25519']
+                destination = tx_operation['body']['paymentOp']['destination']['ed25519']
+                amount = tx_operation['body']['paymentOp']['amount']
 
-                        cur.execute("INSERT INTO payments VALUES (DEFAULT, %s, %s, %s ,%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s))",
-                                    (tx_account,
-                                     tx_account_sequence,
-                                     source,
-                                     destination,
-                                     amount,
-                                     tx_memo,
-                                     tx_fee,
-                                     tx_charged_fee,
-                                     op_status,
-                                     op_index,
-                                     tx_status,
-                                     tx_hash,
-                                     tx_ledger_sequence,
-                                     timestamp))
+                # Override the tx source with the operation source if it exists
+                if len(tx_operation['sourceAccount']) > 0 and 'ed25519' in tx_operation['sourceAccount'][0]:
+                    source = tx_operation['sourceAccount'][0]['ed25519']
 
-                '''
-                # Operation type 0 = Create account
-                elif tx_operation['body']['type'] == 0:
-                    source = transaction['tx']['sourceAccount']['ed25519']
-                    destination = tx_operation['body']['createAccountOp']['destination']['ed25519']
-                    balance = tx_operation['body']['createAccountOp']['startingBalance']
-                    op_status = result_operation['tr']['createAccountResult']['code']
-
-                    # Override the tx source with the operation source if it exists
-                    try:
-                        source = tx_operation['sourceAccount'][0]['ed25519']
-                    except (KeyError, IndexError):
-                        pass
-
-                    cur.execute("INSERT INTO creations VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s));",
-                                (source,
-                                 destination,
-                                 balance,
+                # append to our aggregator
+                payments.append((tx_hash,
+                                 tx_account,
+                                 tx_account_sequence,
+                                 source, destination,
+                                 amount,
                                  tx_memo,
-                                 tx_fee,
-                                 tx_charged_fee,
                                  op_index,
-                                 tx_status,
-                                 op_status,
-                                 tx_hash,
+                                 tx_ledger_sequence,
                                  timestamp))
-                '''
+
+        # Insert aggregated values
+        execute_values(cur, 'INSERT INTO payments (tx_hash, account, account_sequence, source, destination, '
+                            'amount, memo_text, op_index, ledger_sequence, date) VALUES %s ON CONFLICT DO NOTHING',
+                       payments, '(%s, %s, %s, %s, %s, %s, %s, %s, %s, to_timestamp(%s))')
 
     # Update the 'lastfile' entry in the database
     cur.execute("UPDATE lastfile SET name = %s", (file_name,))
+
     conn.commit()
-    logging.info('Successfully wrote contents of file: {} to database'.format(file_name))
+    logging.info('Successfully imported file {} to database: {} records written'.format(file_name, len(payments)))
 
 
 def get_new_file_sequence(old_file_name):
@@ -246,14 +250,9 @@ def get_new_file_sequence(old_file_name):
 def main():
     """Main entry point."""
     # Initialize everything
-    if APP_ID is not None:
-        if re.match('^[A-z0-9]{4}$', APP_ID) is None:
-            logging.error('APP ID is invalid')
-            sys.exit(1)
-
     conn = setup_postgres()
     cur = conn.cursor()
-    file_sequence = '00203dff' #get_last_file_sequence(conn, cur)
+    file_sequence = get_last_file_sequence(conn, cur)
     s3 = setup_s3()
 
     while True:
@@ -268,25 +267,27 @@ def main():
             logging.info('Stopped by user, exiting')
             exit(0)
 
+        results_file = 'results-{}.xdr.gz'.format(file_sequence)
+        ledger_file = 'ledger-{}.xdr.gz'.format(file_sequence)
+        transaction_file = 'transactions-{}.xdr.gz'.format(file_sequence)
+
         # Unpack the files
-        results = parser.parse('results-{}.xdr.gz'.format(file_sequence))
-        ledgers = parser.parse('ledger-{}.xdr.gz'.format(file_sequence))
-        transactions = parser.parse('transactions-{}.xdr.gz'.format(file_sequence),
-                                    with_hash=True, network_id=NETWORK_PASSPHARSE)
+        results = xdrparser.parse(results_file)
+        ledgers = xdrparser.parse(ledger_file)
+        transactions = xdrparser.parse(transaction_file, with_hash=True, network_id=NETWORK_PASSPHRASE)
 
-        # Get a ledger:closeTime dictionary
+        # Build dictionaries
         ledgers_dictionary = get_ledgers_dictionary(ledgers)
-        # Get a txHash:txResult dictionary
         results_dictionary = get_result_dictionary(results)
-
-        # Remove the files from storage
-        logging.info('Removing downloaded files.')
-        os.remove('ledger-{}.xdr.gz'.format(file_sequence))
-        os.remove('transactions-{}.xdr.gz'.format(file_sequence))
-        os.remove('results-{}.xdr.gz'.format(file_sequence))
 
         # Write the data to the postgres database
         write_to_postgres(conn, cur, transactions, ledgers_dictionary, results_dictionary, file_sequence)
+
+        # Remove the files from storage
+        logging.info('Removing downloaded files.')
+        os.remove(ledger_file)
+        os.remove(transaction_file)
+        os.remove(results_file)
 
         # Get the name of the next file I should work on
         file_sequence = get_new_file_sequence(file_sequence)
